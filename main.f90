b98a3fa3753edd6257b42ad0fd7b699e83e5a5a2
Marshall Ward <marshall.ward@gmail.com>
2023-08-02 15:03:35 -0400

    Minor changes

--- main.f90
+++ main.f90
@@ -1,5 +1,5 @@
 use fma_test, only : do_fma
-use fma_test, only : do_prod, do_prod2
+use fma_test, only : do_prod, do_prod2, do_prod3
 
 a = 1.1
 b = 2.2
@@ -16,7 +16,7 @@ print *, "p1(a,b,c,d):", f
 print '(z16)', f
 
 f = do_prod(c,d,a,b)
-print *, "p1(c,d,a,b):", do_prod(c,d,a,b)
+print *, "p1(c,d,a,b):", f
 print '(z16)', f
 
 f = do_prod2(a,b,c,d)
@@ -24,6 +24,14 @@ print *, "p2(a,b,c,d):", f
 print '(z16)', f
 
 f = do_prod2(c,d,a,b)
+print *, "p2(c,d,a,b):", f
+print '(z16)', f
+
+f = do_prod3(a,b,c,d)
+print *, "p2(a,b,c,d):", f
+print '(z16)', f
+
+f = do_prod3(c,d,a,b)
 print *, "p2(c,d,a,b):", f
 print '(z16)', f
 
